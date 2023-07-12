279c7463713ae9a04f595d2023351553ebbfebda
OlexTS <sashokseven@gmail.com>
2023-07-12 21:03:59 +0300

    completed 2nd lesson

--- app.ts
+++ app.ts
@@ -1,14 +1,111 @@
+/*
+  |==============================
+  | TYPESCRIPT: Lesson1
+  |==============================
+*/
+export {};
 const button = document.querySelector("button")! as HTMLButtonElement;
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-
 
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
 
+/*
+  |==============================
+  | Lesson2
+  |==============================
+*/
+
+let age: number;
+let name: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (a: number) => number;
+age = 50;
+name = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = (a) => {
+  return 100 + a;
+};
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+let str: string;
+if (typeof some === "string") {
+  str = some;
+}
+
+let person: [string, number];
+person = ["Max", 21];
+
+enum Status {
+  LOADING,
+  READY,
+}
+const page = {
+  status: Status.READY,
+};
+if (page.status === Status.LOADING) {
+  console.log("Page is loading");
+}
+if (page.status === Status.READY) {
+  console.log("Page is ready");
+}
+
+let status: string | number;
+let status2: "enable" | "disable";
+function showMessage(message: string) {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+type page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {    
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2:  page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
+page2.details = {createAt: new Date, updateAt: new Date}
